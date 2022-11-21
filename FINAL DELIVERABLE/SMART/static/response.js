c1c76083d8aa8f53e4694e8c1d157aefa4be4640
Shijujercy <113982203+Shijujercy@users.noreply.github.com>
2022-11-22 00:43:48 +0530

    Create response.js

---
+++ FINAL DELIVERABLE/SMART/static/response.js
@@ -0,0 +1,10 @@
+const responseObj = {
+    hello: "Hey ! How are you doing ?",
+    hey: "Hey! What's Up",
+    today: new Date().toDateString(),
+    time: new Date().toLocaleTimeString(),
+    hi: "hey",
+    ll: "",
+    types: "Shirt | Chudidhar | Jerkin",
+    mail: "Email id:tt@gmail.com",
+  };
